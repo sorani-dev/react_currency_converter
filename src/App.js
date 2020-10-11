8309986355ef6abc46e43a1ef3cf98d93d52e6ef
simon441 <simon.cateau@yahoo.fr>
2020-10-12 00:53:00 +0200

    Change select value on selected currency option.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,31 @@ const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
-  console.log(currencyOptions)
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
       })
     return () => {}
   }, [])
+
   return (
     <Fragment>
       <h1>Converter App</h1>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+      />
       <div className="equals">=</div>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={toCurrency} onChangeCurrency={e => setToCurrency(e.target.value)} />
     </Fragment>
   )
 }
--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function CurrencyRow({ currencyOptions }) {
+export default function CurrencyRow({ currencyOptions, selectedCurrency, onChangeCurrency }) {
   return (
     <div>
       <input type="number" className="input" />
-      <select>
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map(option => (
           <option key={option} value={option}>
             {option}
